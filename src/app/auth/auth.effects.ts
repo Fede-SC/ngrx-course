42b01b95f10637504177a99042904a6cdd112062
fedesc <fedescivales@gmail.com>
2022-01-03 21:05:27 +0100

    Inplementation of side effectngrx effects

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,16 +1,37 @@
 import { Injectable } from "@angular/core";
-import { act, Actions } from "@ngrx/effects";
+import { act, Actions, createEffect, ofType } from "@ngrx/effects";
+import { tap } from "rxjs/operators";
+import { AuthActions } from "./action-types";
 
 // In questo caso vogliamo che come side effect venga salvato
 // il profilo dell'utente nel local storage
 @Injectable()
 export class AuthEffects {
+  login$ = createEffect(() =>
+    this.actions$
+      .pipe(
+        ofType(AuthActions.login),
+        tap(action => {
+          localStorage.setItem('user', JSON.stringify(action.user));
+        })
+      // specifichiamo che questo particolare side effect non
+      // è un risultato di un dispatch
+    ), {dispatch: false});
   constructor(private actions$: Actions) {
-    actions$.subscribe(action => {
-      // se si tratta dell'azione di login
-      if (action.type === '[Login Page] User Login') {
-        localStorage.setItem('user', JSON.stringify(action['action']));
-      }
-    });
+    // actions$.subscribe(action => {
+    //   // se si tratta dell'azione di login
+    //   if (action.type === '[Login Page] User Login') {
+    //     localStorage.setItem('user', JSON.stringify(action['user']));
+    //   }
+    // });
+    // Le istruzioni sopra equivalgono a quelle seguenti
+    // const login$ = this.actions$
+    //   .pipe(
+    //     ofType(AuthActions.login),
+    //     tap(action => {
+    //       localStorage.setItem('user', JSON.stringify(action.user));
+    //     })
+    //   );
+    // login$.subscribe();
   }
 }
