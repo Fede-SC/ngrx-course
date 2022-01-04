cd91733da17b6ab19ad783d0a2beb44339140d5c
fedesc <fedescivales@gmail.com>
2022-01-04 17:18:55 +0100

    ngrx entity

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -1,15 +1,15 @@
 import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector, createReducer,
-  createSelector,
-  MetaReducer, on
+    ActionReducer,
+    ActionReducerMap,
+    createFeatureSelector, createReducer,
+    createSelector,
+    MetaReducer, on
 } from '@ngrx/store';
 import {User} from '../model/user.model';
 import {AuthActions} from '../action-types';
 
 export interface AuthState {
-  user: User
+    user: User
 }
 
 export const initialAuthState: AuthState = {
@@ -23,11 +23,9 @@ export const authReducer = createReducer(
   initialAuthState,
 
   on(AuthActions.login, (state, action) => {
-    state.user = action.user;
-    return state;
-    // return {
-    //   user: action.user
-    // }
+    return {
+      user: action.user
+    }
   }),
 
   on(AuthActions.logout, (state, action) => {
